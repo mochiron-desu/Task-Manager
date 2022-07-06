644dc330509ef03ce23fbdc2980c348ab141980e
Suraj S <qwerky2003@gmail.com>
2022-07-06 15:45:42 +0530

    Adding changes

--- script.js
+++ script.js
@@ -13,11 +13,19 @@ document.querySelector('#push').onclick = function () {
             </button>
         </div>
         `;
+        document.querySelector('#newtask input').value=""
 
         var current_tasks = document.querySelectorAll(".delete");
         for (var i = 0; i < current_tasks.length; i++) {
             current_tasks[i].onclick = function () {
                 this.parentNode.remove()
+            }
+        }
+
+        var tasks = document.querySelectorAll(".task");
+        for (var i = 0; i < tasks.length; i++) {
+            tasks[i].onclick = function () {
+                this.classList.toggle('completed');
             }
         }
     }
